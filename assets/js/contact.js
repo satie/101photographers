2bd0a3472154f785fd8d12a74adfcb2a0b7d883e
satie <satiesharma@gmail.com>
2017-08-16 10:54:23 -0400

    Show confirmation message on contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -16,8 +16,11 @@ $('#contactForm').submit(function(event) {
     crossDomain: true,
     data: JSON.stringify(contactData)
   }).done(function(response) {
+    $form.css('visibility', 'hidden');
+    $('span.message').css('visibility', 'visible').fadeIn(100);
     console.log(response);
   }).fail(function(error) {
+    $('span.message').css('visibility', 'visible').html('Could not send your message. Please try again. Or email <a href="mailto:info@101photographers.in">info@101photographers.in</a>').fadeIn(100);
     console.log(error);
   });
 });
